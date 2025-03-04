284442e80b862c49e6d3418a2e368b7bbbde31fb
NicolayChillo <nicolaychillo13@gmail.com>
2025-03-04 01:33:07 -0500

    Registro y Validar

--- capaDatos/obj/Debug/net8.0/capaDatos.AssemblyInfo.cs
+++ capaDatos/obj/Debug/net8.0/capaDatos.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("capaDatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73c3b6f35d3a3400d3256eb2c988f4229a8031dc")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e17aaef908b8f6475fdba245c57f5aad1bf9130")]
+>>>>>>> 47bfd35 (Registro y Validar)
 [assembly: System.Reflection.AssemblyProductAttribute("capaDatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("capaDatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
