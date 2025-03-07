22afbe5000504aa3f19f2652efa9337f5a2fbcf1
Cepeda Salguero David Gustavo <150314536+davidcepeda1@users.noreply.github.com>
2025-03-07 03:45:43 -0500

    modo oscuro

---
+++ AlquilerVehiculos/wwwroot/js/modoOscuro.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const toggleSwitch = document.getElementById("input");
+    const body = document.body;
+
+    // Cargar preferencia del usuario
+    if (localStorage.getItem("dark-mode") === "enabled") {
+        body.classList.add("dark-mode");
+        toggleSwitch.checked = true;
+    }
+
+    toggleSwitch.addEventListener("change", function () {
+        body.classList.toggle("dark-mode");
+        if (body.classList.contains("dark-mode")) {
+            localStorage.setItem("dark-mode", "enabled");
+        } else {
+            localStorage.setItem("dark-mode", "disabled");
+        }
+    });
+});
